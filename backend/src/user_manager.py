0b2644bb57624e47b712171795d75445d77c75d5
gabriel miranda <gabrielmfern@outlook.com>
2023-03-12 12:47:14 -0300

    feat: make all of the responses be in a JSON format to be easier to treat in the front-end

--- backend/src/user_manager.py
+++ backend/src/user_manager.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, json, jsonify, request
 from user import User
 import user
 
@@ -19,45 +19,84 @@ def get_users():
 def get_user(id: str):
     resulting_user = user.get_by_id(id)
     if resulting_user is not None:
-        return jsonify(resulting_user), 200
+        return jsonify({
+            "users": resulting_user,
+            "status": "success",
+        }), 200
     else:
-        return f"Could not find a user with the id {id}!", 404
+        return jsonify({
+            "message": f"Could not find a user with the id {id}!",
+            "status": "error"
+        }), 404
 
 @app.delete("/users/<id>")
 def delete_user(id: str):
     result = user.delete(id)
     if result is not None:
-        return jsonify(result), 200
+        return jsonify({
+            "user": result,
+            "status": "success"
+        }), 200
     else:
-        return f"Could not find a user with the id {id}!", 404
+        return jsonify({
+            "message": f"Could not find a user with the id {id}!",
+            "status": "error"
+        }), 404
 
 @app.put("/users")
 def update_user():
     new_user_data: dict = dict(request.get_json())
     if new_user_data is not None:
         if "id" not in new_user_data:
-            return "Missing 'id' in the update body!", 400
+            return jsonify({
+                "message": "Missing 'id' in the update body!",
+                "status": "error"
+            }), 400
 
         result = user.update(User.from_dict(new_user_data))
 
         if result is not None:
-            return f"Successfully updated the User with id {new_user_data['id']}", 200
+            return jsonify({
+                "message": f"Successfully updated the User with id {new_user_data['id']}",
+                "status": "success"
+            }), 200
         else:
-            return f"Could not find a user with the id {id}!", 404
+            return jsonify({
+                "message": f"Could not find a user with the id {id}!",
+                "status": "error"
+            }), 404
     else:
-        return "Missing body in the update request!", 400
+        return jsonify({
+            "message": "Missing body in the update request!",
+            "status": "error"
+        }) , 400
 
 @app.post("/users")
 def create_user():
     user_data: dict = dict(request.get_json())
     if user_data is not None:
         if "name" not in user_data:
-            return "Missing name for the new user!", 400
+            return jsonify({
+                "message": "Missing name for the new user!",
+                "status": "error"
+            }), 400
         elif "email" not in user_data:
-            return "Missing email for the new user!", 400
+            return jsonify({
+                "message": "Missing email for the new user!",
+                "status": "error"
+            }), 400
         elif "role" not in user_data:
-            return "Missing role for the new user!", 400
+            return jsonify({
+                "message": "Missing role for the new user!",
+                "status": "error"
+            }), 400
 
-        return user.create(User.from_dict(user_data)), 200
+        return jsonify({
+            "user_id": user.create(User.from_dict(user_data)),
+            "status": "success"
+        }), 200
     else:
-        return "Missing body in the user creation request!", 400
+        return jsonify({
+            "message": "Missing body in the user creation request!",
+            "status": "error"
+        }), 400
